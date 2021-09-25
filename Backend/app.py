9517f75faae8ef219983a89ba740d341b5335762
Nipat <nipat.phattarakijtham@mail.kmutt.ac.th>
2021-09-25 22:26:54 +0700

    Up Vel

--- Backend/app.py
+++ Backend/app.py
@@ -1,56 +1,23 @@
 from flask import Flask
 import pickle
 import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
+from model_util import make_prediction
 import os
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-model_folder = os.path.join(BASE_DIR,'Backend/model/')
-model_file = os.path.join(model_folder,'model.sav')
-
-# Import CosineSimModel
-# class CosineSimModel:
-#   def __init__(self):
-#     self.data = []
-#   def predict(self,X):
-#     maxind = 0
-#     maxval = 0
-#     for i in X:
-#       for j in range(len(self.data)):
-#         tempY = self.data[j]['X'].reshape(1,-1)
-#         cs = cosine_similarity(X,tempY)
-#         if cs > maxval:
-#           maxval = cs
-#           maxind = j
-#           print(maxval)
-#           print(self.data[j]['y'])
-#     return str(self.data[maxind]['y'])
-#   def fit(self,X,y):
-#     for i in range(len(X)):
-#       self.data.append({'X':np.array(X.iloc[i,:]),'y':np.array(y.iloc[i])})
-
-# def make_prediction():
-#     loaded_model = pickle.load(open('model/model.pkl', 'rb'))
-#     result = loaded_model.predict([[1,1,1,4,2,3,1,1,0,5]])
-#     print(result)
-#     return ""
-
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
-    loaded_model = pickle.load(open('model/model.pkl'),'rb')
-    print(loaded_model)
-    
 
+    
 @app.route("/")
 def hello_world():
     return "<p>Hello world</p>"
 
 @app.route('/api',methods=['GET'])
 def api():
-    # temp = make_prediction()
-    # print(temp)
-    write = "<p>This is api</p>"
+    result = make_prediction()
+    print(result)
+    write = "<p>The result of this prediction is "+ result +"</p>"
     return write
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
---
+++ Backend/model_util.py
@@ -0,0 +1,45 @@
+import pickle
+import os
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+model_folder = os.path.join(BASE_DIR,'Backend/model/')
+model_file = os.path.join(model_folder,'model.sav')
+
+class CosineSimModel:
+  def __init__(self):
+    self.data = []
+  def predict(self,X):
+    maxind = 0
+    maxval = 0
+    for i in X:
+      for j in range(len(self.data)):
+        tempY = self.data[j]['X'].reshape(1,-1)
+        cs = cosine_similarity(X,tempY)
+        if cs > maxval:
+          maxval = cs
+          maxind = j
+        #   print(maxval)
+        #   print(self.data[j]['y'])
+    return str(self.data[maxind]['y'])
+  def fit(self,X,y):
+    for i in range(len(X)):
+      self.data.append({'X':np.array(X.iloc[i,:]),'y':np.array(y.iloc[i])})
+
+
+class CustomUnpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == "__main__":
+            module = "model_util"
+        return super().find_class(module, name)
+
+with open('model/model.pkl','rb') as f:
+    unpickler = CustomUnpickler(f)
+    loaded_model = unpickler.load()
+    print(loaded_model)
+
+def make_prediction(data_input):
+    result = loaded_model.predict([[1,1,1,4,2,3,1,1,0,5]])
+    return result
+
