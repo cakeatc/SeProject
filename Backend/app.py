d7487ff5b5d431dbd9afcd940b3b3dedb55f5a8a
Nipat <nipat.phattarakijtham@mail.kmutt.ac.th>
2021-09-25 22:02:19 +0700

    update

--- Backend/app.py
+++ Backend/app.py
@@ -9,38 +9,40 @@ model_folder = os.path.join(BASE_DIR,'Backend/model/')
 model_file = os.path.join(model_folder,'model.sav')
 
 # Import CosineSimModel
-class CosineSimModel:
-  def __init__(self):
-    self.data = []
-  def predict(self,X):
-    maxind = 0
-    maxval = 0
-    for i in X:
-      for j in range(len(self.data)):
-        tempY = self.data[j]['X'].reshape(1,-1)
-        cs = cosine_similarity(X,tempY)
-        if cs > maxval:
-          maxval = cs
-          maxind = j
-          print(maxval)
-          print(self.data[j]['y'])
-    return str(self.data[maxind]['y'])
-  def fit(self,X,y):
-    for i in range(len(X)):
-      self.data.append({'X':np.array(X.iloc[i,:]),'y':np.array(y.iloc[i])})
+# class CosineSimModel:
+#   def __init__(self):
+#     self.data = []
+#   def predict(self,X):
+#     maxind = 0
+#     maxval = 0
+#     for i in X:
+#       for j in range(len(self.data)):
+#         tempY = self.data[j]['X'].reshape(1,-1)
+#         cs = cosine_similarity(X,tempY)
+#         if cs > maxval:
+#           maxval = cs
+#           maxind = j
+#           print(maxval)
+#           print(self.data[j]['y'])
+#     return str(self.data[maxind]['y'])
+#   def fit(self,X,y):
+#     for i in range(len(X)):
+#       self.data.append({'X':np.array(X.iloc[i,:]),'y':np.array(y.iloc[i])})
 
-def make_prediction():
-    loaded_model = pickle.load(open('model/model.pkl', 'rb'))
-    result = loaded_model.predict([[1,1,1,4,2,3,1,1,0,5]])
-    print(result)
-    return ""
+# def make_prediction():
+#     loaded_model = pickle.load(open('model/model.pkl', 'rb'))
+#     result = loaded_model.predict([[1,1,1,4,2,3,1,1,0,5]])
+#     print(result)
+#     return ""
 
 
 app = Flask(__name__)
 
 if __name__ == '__main__':
-    pickle.load('model/model.pkl')
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
+    loaded_model = pickle.load(open('model/model.pkl'),'rb')
+    print(loaded_model)
+    
 
 @app.route("/")
 def hello_world():
